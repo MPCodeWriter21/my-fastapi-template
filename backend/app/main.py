4ffbe7900e8c8afff19d4d42e35b1fcc55053a77
CodeWriter21 <CodeWriter21@gmail.com>
2026-03-16 00:37:03 +0330

    fix: Make sure Swagger UI is available offline
    + Read more: https://github.com/MPCodeWriter21/journal

--- backend/app/main.py
+++ backend/app/main.py
@@ -1,14 +1,21 @@
 import sentry_sdk
 from fastapi import FastAPI
+from fastapi.openapi.docs import get_swagger_ui_html
+from fastapi.responses import HTMLResponse
 from fastapi.routing import APIRoute
+from fastapi.staticfiles import StaticFiles
 from starlette.middleware.cors import CORSMiddleware
 
 from app.api.main import api_router
 from app.core.config import settings
 
+OPENAPI_URL = f"{settings.API_V1_STR}/openapi.json"
+
 
 def custom_generate_unique_id(route: APIRoute) -> str:
-    return f"{route.tags[0]}-{route.name}"
+    if route.tags:
+        return f"{route.tags[0]}-{route.name}"
+    return route.name
 
 
 if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
@@ -16,8 +23,9 @@ if settings.SENTRY_DSN and settings.ENVIRONMENT != "local":
 
 app = FastAPI(
     title=settings.PROJECT_NAME,
-    openapi_url=f"{settings.API_V1_STR}/openapi.json",
+    openapi_url=OPENAPI_URL,
     generate_unique_id_function=custom_generate_unique_id,
+    docs_url=None,
 )
 
 # Set all CORS enabled origins
@@ -31,3 +39,15 @@ if settings.all_cors_origins:
     )
 
 app.include_router(api_router, prefix=settings.API_V1_STR)
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+
+@app.get("/docs", include_in_schema=False)
+async def custom_swagger_ui_html() -> HTMLResponse:
+    return get_swagger_ui_html(
+        openapi_url=OPENAPI_URL,
+        title="API Docs",
+        swagger_js_url="/static/swagger/swagger-ui-bundle.js",
+        swagger_css_url="/static/swagger/swagger-ui.css",
+    )
